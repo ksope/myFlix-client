63735e40e56e557df4c242c106497e2037f734af
ksope <105855785+ksope@users.noreply.github.com>
2023-05-25 20:33:55 +0100

    Create login-view.jsx component
    Added a login view to allow users to log in to the app

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,49 @@
+import {useState} from 'react';
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    //this prevents default behaviour of the form which is to reload the page
+    e.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://myflixapp-220423.herokuapp.com/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  }).then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
+  };
+
+  return (
+    <form onSubmit= {handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e)=> setUsername(e.target.value)} minLength="8" required />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} required />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
