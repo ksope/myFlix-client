8e857ddd8e3c7dbf91b33574e3ff600bfe792f42
ksope <105855785+ksope@users.noreply.github.com>
2023-05-25 20:41:38 +0100

    Update main-view.jsx file
    Updated the LoginView so that after a successful login, the user object and token will be stored using localStorage
    
    Added movies.filter() method to filter 'movies' array based on genre name.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,25 +1,74 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
 
-useEffect(() => {
-    fetch("https://myflixapp-220423.herokuapp.com/movies")
-      .then((res)=> {
-        const moviesFromApi = res.data;
-        setMovies(moviesFromApi);
+   useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    fetch("https://myflixapp-220423.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((movies) => {
+       setMovies(movies);
       });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+      <LoginView
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }}
+      />
+      or
+      <SignupView />
+      </>
+      
+    );
+  }
 
   if (selectedMovie) {
+    let similarMovies = movies.filter((movie) =>
+        movie.Genres.includes(selectedMovie.Genres[0]) && movie._id !== selectedMovie._id);
+
     return (
+      <>
       <MovieView
         movie={selectedMovie}
         onBackClick={() => setSelectedMovie(null)}
       />
+      <br />
+      <h2>Similar Movies</h2>
+      {similarMovies.length > 0 && (
+        <div>
+                {similarMovies.map((movie) => (
+                    <MovieCard
+                      key={movie._id}
+                      movie={movie}
+                      onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                      }}
+                    />
+                ))}
+                </div>
+            )}
+      </>
+
     );
   }
 
@@ -38,6 +87,7 @@ useEffect(() => {
           }}
         />
       ))}
+      <button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
     </div>
   );
 };
