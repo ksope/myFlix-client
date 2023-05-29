8812e413270fca16a1842173f61d9c2be33deebf
ksope <105855785+ksope@users.noreply.github.com>
2023-05-29 22:23:36 +0100

    Refactored "./index.jsx" file
    Added "./index.scss" to enable bootstrap css styling
    
    Wrapped <MainView /> with <Container></Container> to add responsive grid to app

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,18 @@
 import {createRoot} from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+
 //Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 //Main component (will eventualy use all the others)
 const MyFlixApplication = () => {
     return (
-        <MainView />
-    );
+  <Container>
+    <MainView />
+  </Container>
+);
 };
 
 // Finds the root of your app
