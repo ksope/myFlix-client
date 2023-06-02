0cfa452e00665c7bd76143ba5f6e239f9033ee45
ksope <105855785+ksope@users.noreply.github.com>
2023-06-02 18:11:32 +0100

    Refactored main-view.jsx
    Added the NavigationBar component to the top of <BrowserRouter></BrowserRouter>

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,6 +6,7 @@ import { SignupView } from "../signup-view/signup-view";
 import Row from "react-bootstrap/Row";
 import Col from 'react-bootstrap/Col';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
@@ -31,6 +32,7 @@ export const MainView = () => {
 
 return (
     <BrowserRouter>
+    <NavigationBar />
       <Row className="justify-content-md-center">
         <Routes>
           <Route
