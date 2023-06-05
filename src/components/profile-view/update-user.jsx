6685bcbe4296b097e4b0dcf4dfffcc1f70ce1e94
ksope <105855785+ksope@users.noreply.github.com>
2023-06-05 22:32:48 +0100

    Create update-user.jsx
    Created update-user.jsx component to display the page where a user can update user details

---
+++ src/components/profile-view/update-user.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default function UpdateUser({handleSubmit,handleUpdate}) {
+  return (
+    <div>
+        <form className='profile-form' onSubmit={(e)=>handleSubmit(e)}>
+        <h2>Want to change sme info?</h2>
+        <label>Username:</label>
+        <input
+         type="text"
+         name='Username'
+          defaultValue={user.Username}
+          onChange={(e) => handleUpdate(e.target.value)}
+          required
+          minLength="3"
+        />
+        <label>Password:</label>
+        <input
+         type="password"
+         name='Password'
+          defaultValue={user.Password}
+          onChange={(e) => handleUpdate(e.target.value)}
+          required
+        />
+        <label>Email:</label>
+        <input
+         type="email"
+         name='Email'
+          defaultValue={user.Email}
+          onChange={(e) => handleUpdate(e.target.value)}
+          required
+        />
+        <label>Birthday:</label>
+        <input
+         type="date"
+         name='Birthday'
+          defaultValue={user.Birthday}
+          onChange={(e) => handleUpdate(e.target.value)}
+          required
+        />
+        <button variant='primary' type='submit'>Update</button>
+    </form>
+    </div>
+  )
+}
