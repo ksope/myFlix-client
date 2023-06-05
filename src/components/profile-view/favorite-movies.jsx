1aa67435d9bc3bc86d0cbdfa3ef47820a867217e
ksope <105855785+ksope@users.noreply.github.com>
2023-06-05 22:29:51 +0100

    Create favorite-movies.jsx
    Created favorite-movies.jsx component to return list of favorite movies for user

---
+++ src/components/profile-view/favorite-movies.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function FavoriteMovies({favoriteMovieList}) {
+  return (
+    <div>
+        <h2>Favourite Movies</h2>
+        {favoriteMovieList.map((movies)=> {
+            return(
+                <div key={movies.id}>
+                    <img src={movies.ImagePath} />
+                    <Link to={`/movies/${movies._id}`}>
+                        <h4>{movies.Title}</h4>
+                    </Link>
+                    <button variant="secondary" onClick={()=> removeFav(movies._id)}>Remove from list</button>
+
+                </div>
+            )
+        })}
+    </div>
+  )
+}
